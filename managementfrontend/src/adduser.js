9509fecb8d4f817132c2c7db53c1ecfbb2a30878
Nata Chikhinashvili <natanatuka33@gmail.com>
2023-09-12 14:20:11 +0400

    feat: update api

--- managementfrontend/src/adduser.js
+++ managementfrontend/src/adduser.js
@@ -1,18 +1,33 @@
 import api from './api/axiosConfig';
+import React, { useState } from 'react';
 
 function Adduser() {
 
-  function handleSubmit(event){
-    event.preventDefault();
+  const [formData, setFormData] = useState({});
+
+  async function handleSubmit(event){
+    event.preventDefault();        
+    try {
+            const response = await api.post('/api/add', formData);
+            console.log('Server response:', response.data);
+            // You can handle the response data here
+        } catch (error) {
+            console.error('Error:', error);
+        }
   }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
     return (
         <div id="postcontainer">
             <form method="post">
                 <label>name</label>
-                <input name="name"></input>
+                <input name="name" onChange={handleChange}></input>
                 <label>language</label>
-                <input name="language"></input>
+                <input name="language" onChange={handleChange}></input>
                 <button onClick={handleSubmit}>submit</button>
             </form>
         </div>
